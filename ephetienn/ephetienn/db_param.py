2c33ebdec9e3e728a7298dd6691bd9ddace74e46
FrancoisT97 <56149458+FrancoisT97@users.noreply.github.com>
2020-03-11 15:18:38 +0100

    correction bug insert dans la db

--- ephetienn/ephetienn/db_param.py
+++ ephetienn/ephetienn/db_param.py
@@ -5,28 +5,23 @@ mydb = mysql.connector.connect(
     user="root",
     passwd="root",
     database="db_ephetienn"
-    )
+)
 
 mycursor = mydb.cursor()
 
 
-
-def selectDb(nomDb):
-
+def afficherInfoDb(nomDb):
     sql = ("Select * from {0}").format(nomDb)
 
     mycursor.execute(sql)
 
-
     myresult = mycursor.fetchall()
 
     for x in myresult:
-      print(x)
+        print(x)
 
 
-def insertDb(nom, mdp):
-
-
+def insertUser(nom, mdp):
     sql = "INSERT INTO user (name, pswd) VALUES (%s, %s)"
     val = (nom, mdp)
 
@@ -37,7 +32,7 @@ def insertDb(nom, mdp):
     print(mycursor.rowcount, "record inserted.")
 
 
-def updateDb(id):
+def updateUser(id):
     sql = ("UPDATE user SET name = 'Canyon' WHERE id = {0}").format(id)
 
     mycursor.execute(sql)
@@ -47,14 +42,27 @@ def updateDb(id):
     print(mycursor.rowcount, "record(s) affected")
 
 
-def deleteUser(id):
-    sql = ("DELETE FROM user WHERE id = {0}").format(id)
+def deleteData(nomDb, id):
+    sql = ("DELETE FROM {0} WHERE id = {1}").format(nomDb, id)
 
     mycursor.execute(sql)
 
     mydb.commit()
 
     print(mycursor.rowcount, "record(s) deleted")
+
+
+def insertEvent(date, event, description):
+    sql = ("INSERT INTO event (dateEvent, nameEvent, descEvent) VALUES ('{0}', '{1}', '{2}')").format(date, event, description)
+    # date au format '2020-03-11'
+
+
+    print(sql)
+    mycursor.execute(sql)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
 
 
 
